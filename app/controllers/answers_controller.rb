c96cb457054cc297eadbc9ae48d845865b983114
victorgeai <victor.geai@gmail.com>
2020-05-05 21:19:34 +0200

    stupid coaching behavior ok, testing to be made later

---
+++ app/controllers/answers_controller.rb
@@ -0,0 +1,10 @@
+class AnswersController < ApplicationController
+  def answer
+    if params[:question] == 'I am going to work'
+      @answer = 'Great!'
+    elsif params[:question].include?('?')
+      @answer = 'Silly question, get dressed and go to work!'
+    else @answer = "I don't care, get dressed and go to work!"
+    end
+  end
+end
